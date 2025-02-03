38c61cd8369f1c115979ffce2d1f4081ca310ded
juriikim <joyyjoin@gmail.com>
2025-02-03 22:26:14 +0900

    Feat: 사용자 위치 중심으로 지도 표현

--- components/layout/kakaoMap.tsx
+++ components/layout/kakaoMap.tsx
@@ -1,25 +1,41 @@
 "use client";
 import Script from "next/script";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 declare global {
   interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     kakao: any;
   }
 }
 
 export default function KakaoMap() {
-  const mapRef = useRef(null);
+  const mapDivRef = useRef(null);
+  const defaultPosition = { x: 37.578611, y: 126.977222 };
+  const mapRef = useRef<any>(null);
+
+  const moveMap = (x: number, y: number) => {
+    const moveLocation = new window.kakao.maps.LatLng(x, y);
+    mapRef.current?.panTo(moveLocation);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      moveMap(coords.latitude, coords.longitude);
+    });
+  }, []);
+
   return (
     <>
-      <div ref={mapRef} className="h-full w-full"></div>
+      <div ref={mapDivRef} className="h-full w-full"></div>
       <Script
         src={process.env.NEXT_PUBLIC_MAP_URL}
         onLoad={() => {
           window.kakao.maps.load(() => {
-            new window.kakao.maps.Map(mapRef.current, {
-              center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            mapRef.current = new window.kakao.maps.Map(mapDivRef.current, {
+              center: new window.kakao.maps.LatLng(
+                defaultPosition.x,
+                defaultPosition.y,
+              ),
               level: 3,
             });
           });
