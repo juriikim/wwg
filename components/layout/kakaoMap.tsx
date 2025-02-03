d84beb11d00d04f5ec81348242cfb67038a00cfc
juriikim <joyyjoin@gmail.com>
2025-02-03 22:41:13 +0900

    Feat: 지도 마커 추가

--- components/layout/kakaoMap.tsx
+++ components/layout/kakaoMap.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import Script from "next/script";
 import { useEffect, useRef } from "react";
@@ -12,6 +13,14 @@ export default function KakaoMap() {
   const mapDivRef = useRef(null);
   const defaultPosition = { x: 37.578611, y: 126.977222 };
   const mapRef = useRef<any>(null);
+  const marker = useRef<any>(null);
+
+  const drawMarker = (x: number, y: number) => {
+    marker.current = new window.kakao.maps.Marker({
+      position: new window.kakao.maps.LatLng(x, y),
+    });
+    marker.current.setMap(mapRef.current);
+  };
 
   const moveMap = (x: number, y: number) => {
     const moveLocation = new window.kakao.maps.LatLng(x, y);
@@ -21,6 +30,7 @@ export default function KakaoMap() {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords }) => {
       moveMap(coords.latitude, coords.longitude);
+      drawMarker(coords.latitude, coords.longitude);
     });
   }, []);
 
